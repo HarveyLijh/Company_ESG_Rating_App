8ff3ba90fd4fd6253384782eac0cea2885232854
HarveyLijh <43796904+HarveyLijh@users.noreply.github.com>
2020-09-05 17:54:13 -0500

    Updates

---
+++ precache-manifest.cdcda1fa59ff23c5c96635003c0153e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09b0149df5d7ad939ad069730e44ea9",
+    "url": "/Company_ESG_Rating_App/index.html"
+  },
+  {
+    "revision": "3b3ef3054ee01f471e63",
+    "url": "/Company_ESG_Rating_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45eafd24888fbdab437a",
+    "url": "/Company_ESG_Rating_App/static/js/2.39942fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Company_ESG_Rating_App/static/js/2.39942fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3ef3054ee01f471e63",
+    "url": "/Company_ESG_Rating_App/static/js/main.86aeb181.chunk.js"
+  },
+  {
+    "revision": "e3835ee334378babef88",
+    "url": "/Company_ESG_Rating_App/static/js/runtime-main.776abe2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Company_ESG_Rating_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
