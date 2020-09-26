91a3698ac2ec1fbe3f2c1813466774499d3cf4eb
HarveyLijh <43796904+HarveyLijh@users.noreply.github.com>
2020-09-25 22:41:02 -0500

    shards deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "./assets/style.css";
-import Dashboard from "./components/Dashboard"
-class App extends Component {
-   
-    render() {
-        return (
-            <div className="container">
-                <div className="title">ESG_Rating_App</div>
-                <Dashboard></Dashboard>
-            </div>
-        );
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
