7ab82c2dab6b680efbc9b3ec3dc28060cbb0e0a7
HarveyLijh <43796904+HarveyLijh@users.noreply.github.com>
2020-09-27 02:33:18 -0500

    let the data flow in
    input CEIS and weights csv and transfer to json successfully

--- src/App.js
+++ src/App.js
@@ -1,34 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 // import { MemoryRouter as Router, Route } from "react-router-dom";
 
 // import routes from "./routes";
 import Company from "./views/Company";
 // import withTracker from "./withTracker";
 import { Container, Row, Col } from "shards-react";
-
 import MainNavbar from "./components/layout/MainNavbar/MainNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 
-const App = () => {
-  // const CEIS_data = 
-  return (
-    <Container fluid>
-      <Row>
+var CEIS = require("./data/CEIS.csv");
+var weights = require("./data/weights.csv");
+var Papa = require("papaparse/papaparse.min.js");
 
-        <Col
-          className="main-content p-0"
-          sm="12"
-          tag="main"
-        >
-          <MainNavbar />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.company_CEIS = null;
+    this.agency_weights = null;
+  }
+  componentDidMount() {
+    var that = this
+    // parse CEIS data
+    Papa.parse(CEIS, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      complete: function (result) {
 
-          <Company />
-          {/* {!noFooter && <MainFooter />} */}
-        </Col>
-      </Row>
-    </Container>
-  );
+        that.setCEIS(result);
+      }
+    });// parse weights data
+    console.log(Papa.parse(weights, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      complete: function (result) {
+
+        that.setWeights(result);
+      }
+    }));
+   
+  }
+   setCEIS(result) {
+    this.company_CEIS = result;
+    console.log(this.company_CEIS)
+  }
+   setWeights(result) {
+    this.agency_weights = result;
+    console.log( this.agency_weights)
+  }
+  render() {
+    //search
+    return (
+      <Container fluid>
+        <Row>
+
+          <Col
+            className="main-content p-0"
+            sm="12"
+            tag="main"
+          >
+            <MainNavbar />
+
+            <Company />
+            {/* {!noFooter && <MainFooter />} */}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default App;
