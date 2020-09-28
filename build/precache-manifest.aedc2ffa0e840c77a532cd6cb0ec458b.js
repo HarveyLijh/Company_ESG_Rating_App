d3026a61c8f99517b47f2369f5dd20fab443d32e
HarveyLijh <43796904+HarveyLijh@users.noreply.github.com>
2020-09-28 14:20:57 -0500

    build

---
+++ build/precache-manifest.aedc2ffa0e840c77a532cd6cb0ec458b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b69550d8353e56b2de1865880e4c74d",
+    "url": "/Company_ESG_Rating_App/static/media/weights.2b69550d.csv"
+  },
+  {
+    "revision": "899764ed6ad597230c1915d20ca0024b",
+    "url": "/Company_ESG_Rating_App/static/media/CEIS.899764ed.csv"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/Company_ESG_Rating_App/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ca66fbc0f5335e23b3ab",
+    "url": "/Company_ESG_Rating_App/static/js/runtime~main.ca66fbc0.js"
+  },
+  {
+    "revision": "7494eaa236b91814f28e",
+    "url": "/Company_ESG_Rating_App/static/js/main.7494eaa2.chunk.js"
+  },
+  {
+    "revision": "ea8d4786276a9c178969",
+    "url": "/Company_ESG_Rating_App/static/js/1.ea8d4786.chunk.js"
+  },
+  {
+    "revision": "7494eaa236b91814f28e",
+    "url": "/Company_ESG_Rating_App/static/css/main.03e355b2.chunk.css"
+  },
+  {
+    "revision": "ea8d4786276a9c178969",
+    "url": "/Company_ESG_Rating_App/static/css/1.d585bb9b.chunk.css"
+  },
+  {
+    "revision": "5e27b0b0b89a5ecc9129392e82242f5c",
+    "url": "/Company_ESG_Rating_App/index.html"
+  }
+];
